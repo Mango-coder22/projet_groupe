589823079915b4d13133a4c16ed73b031e18df80
mangocoder22 <fcukinmango@gmail.com>
2023-07-12 10:21:53 +0200

    test_commit

---
+++ catalogue.php
@@ -0,0 +1,58 @@
+<?php
+include("header.php")
+;?>
+  <title>catalogue page</title>
+<body class="body">
+      <p id="catalogue_">Catalogue :</p>
+      <div class="anime_filtre">
+        <label class="inline-flex items-center m-1"><input type="checkbox" value="Anime" class="form-checkbox h-5 w-5"><span class="ml-2 text-white font-medium text-md w-24">Anime</span></label>
+        <label class="inline-flex items-center m-1"><input type="checkbox" value="Anime" class="form-checkbox h-5 w-5"><span class="ml-2 text-white font-medium text-md w-24">Vostfr</span></label>
+        <label class="inline-flex items-center m-1"><input type="checkbox" value="Anime" class="form-checkbox h-5 w-5"><span class="ml-2 text-white font-medium text-md w-24">Vf</span></label>
+        <label class="inline-flex items-center m-1"><input type="checkbox" value="Anime" class="form-checkbox h-5 w-5"><span class="ml-2 text-white font-medium text-md w-24">Films</span></label>
+        <label class="inline-flex items-center m-1"><input type="checkbox" value="Anime" class="form-checkbox h-5 w-5"><span class="ml-2 text-white font-medium text-md w-24">Scan</span></label>
+      </div>
+      <main class="page">
+        <div class="div-all_anime_catalogue">
+          <a href="#" class="preview_anime">
+            <div class="co">
+              <div class="car">
+                <img src="img/logoonepiece.png" style="width: 25%">
+                <h2>Overlord</h2>
+                <p>15:30</p>
+              </div>
+            </div></a>
+          <a href="#" class="preview_anime">
+            <div class="co">
+              <div class="car">
+                <img src="img/logoonepiece.png" style="width: 25%">
+                <h2>Overlord</h2>
+                <p>15:30</p>
+              </div>
+            </div></a>
+            <a href="#" class="preview_anime">
+              <div class="co">
+                <div class="car">
+                  <img src="img/logoonepiece.png" style="width: 25%">
+                  <h2>Overlord</h2>
+                  <p>15:30</p>
+                </div>
+              </div></a>
+              <a href="#" class="preview_anime">
+                <div class="co">
+                  <div class="car">
+                    <img src="img/logoonepiece.png" style="width: 25%">
+                    <h2>Overlord</h2>
+                    <p>15:30</p>
+                  </div>
+                </div></a>
+                                    
+                    <!-- <div class="anime_preview"></div>
+                    <div class="anime_preview"></div>
+                    <div class="anime_preview"></div>
+                    <div class="anime_preview"></div>
+                    <div class="anime_preview"></div> -->
+                </div>
+              </main>
+              <?php
+include("footer.php")
+;?>
